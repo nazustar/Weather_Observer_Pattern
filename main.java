ae8718e28c2f26830e1b92a38dadffc62e4337a4
Xiomara ! <xiomarafuentes1811@gmail.com>
2026-02-17 15:57:41 -0500

    refacto: statistics in main

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import display.CurrentConditions;
 import display.ForecastDisplay;
 import display.HeatIndexDisplay;
 import display.PressureDisplay;
-import display.StadisticsDisplay;
+import display.StatisticsDisplay;
 import subject.WeatherData;
 
 public class main {
@@ -14,7 +14,7 @@ public static void main(String[] args) {
     new HeatIndexDisplay(weatherData);
     new PressureDisplay(weatherData);
     new ForecastDisplay(weatherData);
-    new StadisticsDisplay(weatherData);
+    new StatisticsDisplay(weatherData);
 
     weatherData.setMeasurements(15, 78, 1024);
     weatherData.setMeasurements(25,64, 1001);
